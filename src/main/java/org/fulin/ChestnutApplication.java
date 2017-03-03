392f809a4afccd307ea9b2389d1993dfb0bfa6af
tangfulin <tangfl@xueqiu.com>
2017-03-03 15:35:14 +0800

    init

---
+++ src/main/java/org/fulin/ChestnutApplication.java
@@ -0,0 +1,12 @@
+package org.fulin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChestnutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChestnutApplication.class, args);
+	}
+}
